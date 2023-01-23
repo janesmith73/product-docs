bf6585e112e086af38ddf50f423e247dc560d7e4
Carolyn Stransky <hello@workwithcarolyn.com>
2023-01-23 19:30:56 +0100

    Make autogenerating docs work

--- sidebars.js
+++ sidebars.js
@@ -1,4 +1,11 @@
-// @ts-check
+// @ts-ignore
+let api = [];
+try {
+  apiSidebar = require("./docs/graphql/sidebar-schema.js");
+} catch (err) {
+  console.warn(`Warning: Missing sidebar file "./docs/graphql/sidebar-schema.js"`);
+}
+
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   mainDocsSidebar: [
@@ -222,7 +229,7 @@ const sidebars = {
       href: "https://cdn.emnify.net/api/doc/index.html",
     },
   ],
-  ...require("./docs/graphql/sidebar-schema.js")
+  ...apiSidebar,
 };
 
 module.exports = sidebars;
