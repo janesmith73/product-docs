0dcbc06f829c27d707fef2fd9dbc335b9d15270f
Carolyn Stransky <hello@workwithcarolyn.com>
2023-02-06 16:59:37 +0100

    Remove unnecessary comments in sidebar.js (#84)

--- sidebars.js
+++ sidebars.js
@@ -209,29 +209,6 @@ const sidebars = {
       label: "API reference",
       href: "https://cdn.emnify.net/api/doc/index.html",
     },
-    // {
-    //   type: "category",
-    //   label: "REST API",
-    //   link: {
-    //     type: "generated-index",
-    //     title: "emnify REST API",
-    //     slug: "rest-api",
-    //   },
-    //   items: [
-    //     {
-    //       type: "doc",
-    //       label: "Getting started",
-    //       id: "rest-api/getting-started",
-    //     },
-    //     "rest-api/authentication",
-    //     "rest-api/sms-operations",
-    //     {
-    //       type: "link",
-    //       label: "API reference",
-    //       href: "https://cdn.emnify.net/api/doc/index.html",
-    //     },
-    //   ],
-    // },
   ],
 };
 
